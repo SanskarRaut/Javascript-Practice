86cd7699a7e0a1cfe98594a99c0d8baaf1e9fd5c
SanskarRaut <94779936+SanskarRaut@users.noreply.github.com>
2024-02-25 12:55:08 +0000

    Variables done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 112244
+let accountEmail = "abc@gmail.com"
+var accountPassword = "password"
+accountCity = "Mumbai"
+let accountState 
+
+console.log(accountId);
+
+// accountId = 12
+accountPassword = "password123"
+accountEmail = "abcd@gmail.com"
+accountCity = "Pune"
+
+console.table([accountEmail, accountPassword, accountCity, accountState])
